78d2531fc2ad8ca9560be4d4b6e820a9158a3140
w0439802 <122113043+w0439802@users.noreply.github.com>
2024-05-30 09:59:45 -0300

    Added final Level, added features

---
+++ images.g.ts
@@ -0,0 +1,281 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . f f f . . . . . 
+. . . . f f f . . . . . 
+. . . f f f . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "ghostBackwards":
+            case "anim4":return [img`
+. . . f f f f f f . . . 
+. . f f b d d b f f . . 
+. f b d d 1 1 d d b f . 
+. f d d 1 1 1 1 d d f . 
+. f d d 1 1 1 1 d d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b b 1 1 b b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . . f . . . . . 
+. . . . . f f . . . . . 
+. . . . . f . . . . . . 
+`, img`
+. . . . . . . . . . . . 
+. . . f f f f f f . . . 
+. . f f b d d b f f . . 
+. f b d d 1 1 d d b f . 
+. f d d 1 1 1 1 d d f . 
+. f d d 1 1 1 1 d d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b b 1 1 b b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . . f . . . . . 
+. . . . . f f . . . . . 
+`, img`
+. . . . . . . . . . . . 
+. . . f f f f f f . . . 
+. . f f b d d b f f . . 
+. f b d d 1 1 d d b f . 
+. f d d 1 1 1 1 d d f . 
+. f d d 1 1 1 1 d d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b b 1 1 b b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . f . . . . . . 
+. . . . . f f . . . . . 
+`, img`
+. . . f f f f f f . . . 
+. . f f b d d b f f . . 
+. f b d d 1 1 d d b f . 
+. f d d 1 1 1 1 d d f . 
+. f d d 1 1 1 1 d d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b b 1 1 b b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . f . . . . . . 
+. . . . . f f . . . . . 
+. . . . . . f . . . . . 
+`];
+            case "ghostRight":
+            case "anim1":return [img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . f f f . . . . . 
+. . . . f f f . . . . . 
+. . . f f f . . . . . . 
+`, img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . f f f . . . . . 
+. . . f f f . . . . . . 
+. . f f f . . . . . . . 
+`, img`
+. . . . f f f f f f . . 
+. . . f f 1 1 1 1 f f . 
+. . f b 1 1 1 1 1 1 b f 
+. . f d b b 1 1 b b d f 
+. . f d c f 1 1 f c d f 
+. . f b d 1 1 1 1 d b f 
+. . . f b 1 b b 1 b f . 
+. . . . f f f f f f . . 
+. . . . . . f f . . . . 
+. . . . f f f . . . . . 
+. . . f f f . . . . . . 
+. . f f . . . . . . . . 
+`, img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . f f f . . . . . 
+. . . f f f . . . . . . 
+. . f f f . . . . . . . 
+`, img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . f f f . . . . . 
+. . . . f f f . . . . . 
+. . . f f f . . . . . . 
+`];
+            case "ghostLeft":
+            case "anim2":return [img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . f f f . . . . 
+. . . . . f f f . . . . 
+. . . . . . f f f . . . 
+`, img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . f f f . . . . 
+. . . . . . f f f . . . 
+. . . . . . . f f f . . 
+`, img`
+. . f f f f f f . . . . 
+. f f 1 1 1 1 f f . . . 
+f b 1 1 1 1 1 1 b f . . 
+f d b b 1 1 b b d f . . 
+f d c f 1 1 f c d f . . 
+f b d 1 1 1 1 d b f . . 
+. f b 1 b b 1 b f . . . 
+. . f f f f f f . . . . 
+. . . . f f . . . . . . 
+. . . . . f f f . . . . 
+. . . . . . f f f . . . 
+. . . . . . . . f f . . 
+`, img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . f f f . . . . 
+. . . . . . f f f . . . 
+. . . . . . . f f f . . 
+`, img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . f f f . . . . 
+. . . . . f f f . . . . 
+. . . . . . f f f . . . 
+`];
+            case "ghostForward":
+            case "anim3":return [img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . . f . . . . . 
+. . . . . f f . . . . . 
+. . . . . f . . . . . . 
+`, img`
+. . . . . . . . . . . . 
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . f . . . . . . 
+. . . . . f f . . . . . 
+`, img`
+. . . . . . . . . . . . 
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . . f . . . . . 
+. . . . . f f . . . . . 
+`, img`
+. . . f f f f f f . . . 
+. . f f 1 1 1 1 f f . . 
+. f b 1 1 1 1 1 1 b f . 
+. f d b b 1 1 b b d f . 
+. f d c f 1 1 f c d f . 
+. f b d 1 1 1 1 d b f . 
+. . f b 1 b b 1 b f . . 
+. . . f f f f f f . . . 
+. . . . . f f . . . . . 
+. . . . . f . . . . . . 
+. . . . . f f . . . . . 
+. . . . . . f . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
